f6e1cab37ead28ff48ba07d9f49ab43b4d16abe8
Eric Wright <eric@discoposse.com>
2013-12-30 20:49:47 -0500

    Added bigmachinemaker script

---
+++ BigMachineMakerScript.ps1
@@ -0,0 +1,43 @@
+# This will trigger some VM deployments from a template followed by powering on, stopping, then destroying the VMs
+
+# Set up our parameters
+
+$Template = ""
+$Datastore = ""
+$VMHost = ""
+$Username = ""
+$Password = ""
+$CounterTotal = ""
+
+# Connect to vCenter
+Connect-VIserver -Server $VMHost -User $Username -Password $Password -Force
+
+for ($counter=1; $counter -le $CounterTotal; $counter++)
+{
+	# Deploy a VM from the template
+	New-VM -Name TEST$counter -VMHost $VMHost -Datastore $Datastore -Location RJL -Template $Template -RunAsync -Confirm:$FALSE 
+}
+
+sleep -seconds 60
+
+for ($counter=1; $counter -le $CounterTotal; $counter++)
+{
+	#Start our VMs
+	Start-VM -VM TEST$counter -RunAsync -Confirm:$FALSE
+}
+
+sleep -seconds 60
+
+for ($counter=1; $counter -le $CounterTotal; $counter++)
+{
+	# Stop all of our VMs
+	Stop-VM -VM TEST$counter -Confirm:$FALSE -Kill -RunAsync
+}
+
+sleep -seconds 60
+
+for ($counter=1; $counter -le $CounterTotal; $counter++)
+{
+	# Delete all of our VMs
+	Remove-VM -VM TEST$counter -Confirm:$FALSE -RunAsync -DeletePermanently
+}
